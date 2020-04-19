ba7254be5464c3b6f66757516d950d8ff73a0b64
alaadachraoui <31924041+alaadachraoui@users.noreply.github.com>
2020-04-19 15:25:09 +0100

    fix page register

--- vendor/friendsofsymfony/user-bundle/Controller/RegistrationController.php
+++ vendor/friendsofsymfony/user-bundle/Controller/RegistrationController.php
@@ -190,10 +190,7 @@ class RegistrationController extends Controller
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
-        return $this->render('@FOSUser/Registration/confirmed.html.twig', array(
-            'user' => $user,
-            'targetUrl' => $this->getTargetUrlFromSession($request->getSession()),
-        ));
+        return $this->render('default/index.html.twig');
     }
 
     /**
