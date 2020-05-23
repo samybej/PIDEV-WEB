b22dd60df4309e70a8f51bf94f649d6254573948
samybej <samy.bejaoui@esprit.tn>
2020-05-23 13:29:50 +0100

    taxi api

---
+++ src/ApiBundle/Controller/ApiReservationController.php
@@ -0,0 +1,79 @@
+<?php
+
+
+namespace ApiBundle\Controller;
+
+
+use EntitiesBundle\Entity\Chauffeur;
+use EntitiesBundle\Entity\Client;
+use EntitiesBundle\Entity\Reservation;
+
+use EntitiesBundle\Entity\Vehicule;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class ApiReservationController extends Controller
+{
+// 01 05 2020 VERSION
+
+    public function getReservationAction($id)
+    {
+        $utilisateur = $this->getUser();
+
+        //$client = $this->getDoctrine()->getRepository(Client::class)->getIdClient($utilisateur);
+        //$reservation = $this->getDoctrine()->getRepository(Reservation::class)->recupererReservation($client[0]);
+        $client = $this->getDoctrine()->getRepository(Client::class)->find($id);
+        $reservation = $this->getDoctrine()->getRepository(Reservation::class)->findByIdClient($client);
+        $normalizer = array(new DateTimeNormalizer("yy-m-d") ,new ObjectNormalizer());
+        $normalizer[1]->setCircularReferenceLimit(2);
+
+        $normalizer[1]->setCircularReferenceHandler(function ($object){
+            return $object->getId();
+        });
+
+
+        $serializer = new Serializer($normalizer,array(new JsonEncoder()));
+        $formatted = $serializer->serialize($reservation,'json');
+
+        return new Response($formatted);
+    }
+
+    public function addReservationAction($depart,$arrive,$id_chauffeur,$idClient)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reservation = new Reservation();
+        $date = new \DateTime();
+
+        $reservation->setDepart($depart);
+        $reservation->setArrive($arrive);
+        $reservation->setDate($date);
+
+
+        $chauffeur = $this->getDoctrine()->getRepository(Chauffeur::class)->find($id_chauffeur);
+        $reservation->setType("Taxi");
+        $reservation->setDistance(RandomCompat_intval(40));
+        $reservation->setTarif(4);
+        $reservation->setIdChauffeur($chauffeur);
+
+        $client = $this->getDoctrine()->getRepository(Client::class)->find($idClient);
+        $reservation->setIdClient($client);
+
+
+        $em->persist($reservation);
+        $em->flush();
+
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($reservation);
+
+        return new JsonResponse($formatted);
+    }
+
+
+}
